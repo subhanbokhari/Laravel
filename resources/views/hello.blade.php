fc061b32611d47214f18a03d549b8af5e91543cf
Syed Subhan Shah Bokhari <subhanbokhari0@gmail.com>
2023-09-21 21:37:29 +0500

    TASK-1(2)

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello, World!</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p> Subhan Bokhari </p>
+    <p>This is the hello.blade.php view template.</p>
+</body>
+</html>
